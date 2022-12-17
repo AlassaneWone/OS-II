64fbec86957b8466779aaa54da35a227b6704c7e
Tony Rasoamanana <tony.rasoa@gmail.com>
2022-12-17 17:30:34 +0100

    Update Massif
    Reprise du projet

---
+++ course.h
@@ -0,0 +1,9 @@
+
+#ifndef PROJET_COURSE_H
+#define PROJET_COURSE_H
+
+#include "voiture.h"
+
+void course(Voiture *v,int numVoiture,int nbrTour,sem_t *sem);
+
+#endif //PROJET_COURSE_H
---
+++ temps.h
@@ -0,0 +1,12 @@
+
+
+#ifndef PROJETOS_TEMPS_H
+#define PROJETOS_TEMPS_H
+
+int genererTemps(int nbrTour);
+int crachTest();
+int getRandomSecteur();
+int stand();
+int randomStandTime();
+
+#endif //PROJETOS_TEMPS_H
